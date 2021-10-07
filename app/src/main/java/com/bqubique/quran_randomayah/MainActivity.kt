1a98a9455bbcf855e9292d491a008c7528a62ef5
bqubique <blinnqipa@gmail.com>
2021-10-07 12:38:57 +0200

    Remove fragment as simpler Activities are easier and recommended by Google.

--- app/src/main/java/com/bqubique/quran_randomayah/MainActivity.kt
+++ app/src/main/java/com/bqubique/quran_randomayah/MainActivity.kt
@@ -1,36 +1,65 @@
 package com.bqubique.quran_randomayah
 
-import android.app.Activity
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import com.bqubique.QuranApplication
+import androidx.activity.ComponentActivity
 import com.bqubique.quran_randomayah.api.QuranApi
 import com.bqubique.quran_randomayah.databinding.ActivityMainBinding
+import com.bqubique.quran_randomayah.model.ArabicAyah
 import com.bqubique.quran_randomayah.model.Ayah
-import com.bqubique.quran_randomayah.service.QuranService
-import dagger.hilt.EntryPoint
-import dagger.hilt.InstallIn
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.EarlyEntryPoint
-import dagger.hilt.android.internal.migration.InjectedByHilt
-import dagger.hilt.android.qualifiers.ActivityContext
-import dagger.hilt.android.scopes.ActivityRetainedScoped
-import dagger.hilt.android.scopes.ActivityScoped
 import kotlinx.coroutines.*
 import javax.inject.Inject
-import javax.inject.Named
 
 
 @AndroidEntryPoint
-class MainActivity  : AppCompatActivity() {
+class MainActivity  : ComponentActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+
+
+    @Inject
+    lateinit var quranApi: QuranApi
+
+    private lateinit var arabicAyah: ArabicAyah
+    private lateinit var englishAyah: Ayah
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        englishAyah = getRandomAyah()
+        arabicAyah = getArabicAyah(englishAyah.verse.verseKey)
+
+        binding.tvVerseKey.text = arabicAyah.verses[0].verseKey
+        binding.tvArabic.text = arabicAyah.verses[0].textUthmani
+        binding.tvEnglish.text = englishAyah.verse.translations[0].text
+
+    }
+
+    private fun getRandomAyah(): Ayah {
+        lateinit var randomAyah: Ayah
+        runBlocking {
+            CoroutineScope(Dispatchers.IO).launch {
+                randomAyah = quranApi.getRandomAyah()
+                Log.d("MAIN ACT", randomAyah.verse.verseKey)
+            }.join()
+        }
+
+
+        return randomAyah
+    }
+
+    private fun getArabicAyah(verseKey: String): ArabicAyah {
+        lateinit var arabicAyah: ArabicAyah
+
+        runBlocking {
+            CoroutineScope(Dispatchers.IO).launch {
+                arabicAyah = quranApi.getArabicAyah(verseKey)
+            }.join()
+        }
+        return arabicAyah
     }
 }
