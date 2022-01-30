58786830a16b3e67bafd11a4a7ca491a5834933f
Blin Qipa <blin@menatcode.it>
2022-01-30 08:37:07 +0100

    Change notification icon

---
+++ app/src/main/java/com/bqubique/quran_randomayah/NotificationsService.kt
@@ -0,0 +1,18 @@
+package com.bqubique.quran_randomayah
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+const val TAG = "NotificationsService"
+
+class NotificationsService : FirebaseMessagingService() {
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+        Log.d(TAG, "onMessageReceived: ${p0.data}")
+    }
+
+}
--- app/src/main/java/com/bqubique/quran_randomayah/view/MainActivity.kt
+++ app/src/main/java/com/bqubique/quran_randomayah/view/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : ComponentActivity() {
             WearApp()
         }
     }
+
 }
 
 @OptIn(ExperimentalWearMaterialApi::class)
